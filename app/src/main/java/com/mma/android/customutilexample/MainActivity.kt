1d65ce6c4a7682e3a9657aa98849ad7762263b23
Khalil Maaref <khalil.maaref.ext@groupe-mma.fr>
2022-10-04 11:52:53 +0200

    editing dependency

--- app/src/main/java/com/mma/android/customutilexample/MainActivity.kt
+++ app/src/main/java/com/mma/android/customutilexample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mma.android.customutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mma.android.customlib.CustomBaseUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CustomBaseUtils().sayHello("Hallo")
     }
 }
